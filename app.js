869868ccd084e596ffde914416963daf3cdbff30
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2023-08-25 18:22:01 +0000

    updated

---
+++ app.js
@@ -0,0 +1,13 @@
+
+Vue.createApp({
+
+    data(){
+        return {
+           
+            title: "Learning Vue 3",
+            description: "Welcome to the Vue 3 crash course!",
+
+        }
+    }
+
+}).mount("#app");
