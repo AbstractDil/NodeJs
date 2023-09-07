59474fb6fa57892253239c141a503905483f8ba7
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2023-09-07 17:18:04 +0000

    updated

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ Vue.createApp({
            
             title: "Learning Vue 3",
             description: "Welcome to the Vue 3 crash course!",
+            textClass : "text-danger",
+            imgUrl : "https://res.cloudinary.com/practicaldev/image/fetch/s--I4FWon9Y--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ll22z8mea106b4vjdjy7.jpeg",
+            btnUrl : "https://vuejs.org",
+            cardWidth : "18",
+            isAlert: false,
+            isAvailable : true
 
         }
     }
