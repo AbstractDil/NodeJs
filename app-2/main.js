3a844424c48d26dd29c4da06c7dc851389400a45
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2023-11-18 07:06:03 +0000

    updated

---
+++ app-2/main.js
@@ -0,0 +1,15 @@
+Vue.createApp({
+
+    data(){
+        return {
+        
+            title: "Vue Js",
+            description: "Vue Js tshirt for women",
+            imageUrl : "https://ih1.redbubble.net/image.397107417.7008/ssrco,slim_fit_t_shirt,womens,fafafa:ca443f4786,front,square_product,600x600.u4.jpg",
+            inventory: 0,
+           
+
+        }
+    }
+
+    }).mount("#app");
